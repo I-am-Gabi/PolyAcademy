2678de8d61a899bb9b4e5a94775037b9106ce60a
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2016-04-20 22:40:53 +0200

    change path

--- app/src/main/java/polytech/unice/fr/polynews/fragment/home/InfoFragment.java
+++ app/src/main/java/polytech/unice/fr/polynews/fragment/home/InfoFragment.java
@@ -1,21 +1,6 @@
 package polytech.unice.fr.polynews.fragment.home;
 
-import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.View;
-
-import polytech.unice.fr.polynews.R;
-import polytech.unice.fr.polynews.fragment.home.PlanningFragment;
-import polytech.unice.fr.polynews.fragment.home.PolytechFragment;
 
 
 /**
