71a1cbf3fe54a5ead5ff2dc193e285bb18cf4931
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2016-05-01 01:34:53 +0200

    try change webChromeClient to solve #29

--- app/src/main/java/polytech/unice/fr/polynews/fragment/calendar/CalendarFragment.java
+++ app/src/main/java/polytech/unice/fr/polynews/fragment/calendar/CalendarFragment.java
@@ -3,10 +3,15 @@ package polytech.unice.fr.polynews.fragment.calendar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import polytech.unice.fr.polynews.R;
 
@@ -24,6 +29,16 @@ public class CalendarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.layout_calendar,container,false);
         WebView calendar = (WebView) v.findViewById(R.id.webview_calendar);
+        calendar.setInitialScale(1);
+        calendar.setWebChromeClient(new WebChromeClient());
+        calendar.getSettings().setAllowFileAccess(true);
+        calendar.getSettings().setPluginState(WebSettings.PluginState.ON);
+        calendar.getSettings().setPluginState(WebSettings.PluginState.ON_DEMAND);
+        calendar.setWebViewClient(new WebViewClient());
+        calendar.getSettings().setJavaScriptEnabled(true);
+        calendar.getSettings().setLoadWithOverviewMode(true);
+        calendar.getSettings().setUseWideViewPort(true);
+
         calendar.getSettings().setJavaScriptEnabled(true);
         calendar.loadData(calendar_html, "text/html", null);
 
