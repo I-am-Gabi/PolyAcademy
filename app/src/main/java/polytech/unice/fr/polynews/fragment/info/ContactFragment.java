278eedb0f866ffb981deb9d6e62ca3f098708faf
I-am-Gabi <gabicavalcantesilva@gmail.com>
2016-05-01 15:43:14 +0200

    merge contact and integration

--- app/src/main/java/polytech/unice/fr/polynews/fragment/info/ContactFragment.java
+++ app/src/main/java/polytech/unice/fr/polynews/fragment/info/ContactFragment.java
@@ -16,13 +16,12 @@ import polytech.unice.fr.polynews.R;
 public class ContactFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//<<<<<<< HEAD
         View v =inflater.inflate(R.layout.layout_contact,container,false);
         WebView contact = (WebView) v.findViewById(R.id.webview_contact);
         contact.loadUrl("https://informatique.polytechnice.fr/jahia/webdav/site/informatique/shared/EC5.html");
-//=======
-//        View v =inflater.inflate(R.layout.layout_contact_assos,container,false);
-//>>>>>>> 3fd5e000f17d4340f6ce8428de4a9786b5aa15e1
+
+        // View v =inflater.inflate(R.layout.layout_contact_assos,container,false);
+
         return v;
     }
 }
